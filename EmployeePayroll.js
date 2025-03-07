fc0ede32eb5b8b42d96341481c7bac644f3c7ca7
Om Nema <134232774+omnema007@users.noreply.github.com>
2025-03-07 11:27:55 +0530

    [Om] Add. Ability to extend Employee Payroll Data to store gender and start date

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,20 +1,21 @@
 
-// UC-12: Create Employee Payroll Class 
 
-// Creating Class EmployeePayrollData to store employee payroll details
+// UC-12 & 13: Extend Employee Payroll Data to store gender and start date
+
 class EmployeePayrollData {
+    // Properties: id, name, salary, gender, startDate
     id;
     salary;
-    gender;
-    startDate;
+    gender; // UC-13: Added gender property
+    startDate; // UC-13: Added startDate property
     
     // Constructor to initialize employee payroll properties
     constructor(params) {
         this.id = params[0];
         this.name = params[1];
         this.salary = params[2];
-        this.gender = params[3];
-        this.startDate = params[4];
+        this.gender = params[3]; // UC-13: Initialize gender property
+        this.startDate = params[4]; // UC-13: Initialize startDate property
     }
 
     // Getter method for name
@@ -28,18 +29,10 @@ class EmployeePayrollData {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
         const empDate = this.startDate === undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
         return "id=" + this.id + ", name=" + this.name + ", salary=" + this.salary + 
-               ", gender=" + this.gender + ", startDate=" + empDate;
+               ", gender=" + this.gender + ", startDate=" + empDate; // Highlight: include gender and startDate in toString
     }
 }
 
-// Creating an EmployeePayrollData object with id, name, and salary
-let employeePayrollData = new EmployeePayrollData([1, "Mark", 30000]);
-console.log(employeePayrollData.toString());
-
-// Updating employee name using setter
-employeePayrollData.name = "John";
-console.log(employeePayrollData.toString());
-
-// Creating another EmployeePayrollData object with all properties
+// Creating an EmployeePayrollData object with id, name, salary, gender, and startDate
 let newEmployeePayrollData = new EmployeePayrollData([2, "Terrisa", 30000, "F", new Date()]);
 console.log(newEmployeePayrollData.toString());
